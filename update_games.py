9217268a330d651eb918a7e05e54db0757032798
SL WADDA <freefier52502@gmail.com>
2026-07-18 18:45:09 +0530

    Updated python script to fetch image and summary

--- update_games.py
+++ update_games.py
@@ -1,14 +1,18 @@
 import feedparser
 import json
 
-# FitGirl ගේ RSS එකෙන් දත්ත ගන්නවා
 url = "https://fitgirl-repacks.site/feed/"
 feed = feedparser.parse(url)
 games = []
 
-# අලුත්ම ගේම් 10 අරන් ෆයිල් එකට දානවා
 for entry in feed.entries[:10]:
-    games.append({"title": entry.title, "link": entry.link})
+    game_data = {
+        "title": entry.title,
+        "link": entry.link,
+        "summary": entry.summary,  # මෙතනින් විස්තරය ගන්නවා
+        "image": entry.media_content[0]['url'] if 'media_content' in entry else "" # මෙතනින් පින්තූරය ගන්නවා
+    }
+    games.append(game_data)
 
 with open('games.json', 'w', encoding='utf-8') as f:
     json.dump(games, f, indent=4)
